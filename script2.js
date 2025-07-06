c262aed977d65a892d0a34e96261a5ac91e54007
ManyToMany <sahar.zs2002@gmail.com>
2025-07-06 10:55:13 +0330

    Numeric seperators

--- script2.js
+++ script2.js
@@ -108,3 +108,22 @@ labelBalance.addEventListener('click', function () {
     }
   );
 });
+
+/////////////////////////////////////////////////
+// lecture: Numeric seperators
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_36;
+console.log(price);
+
+const transferFee1 = 15_2;
+const transferFee2 = 1025_25;
+console.log(transferFee1, transferFee2);
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('235_54')); // NaN
+console.log(parseInt('23_54')); // 23
